9f2491d94a7f8c74ff5a2bc4c9b648d7018acd35
Pitchayapong Wisawachaiyawat <ghmwizzy@gmail.com>
2020-03-26 14:30:50 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Pitchayapong Wisawachaiyawat on 3/26/20.
+//  Copyright © 2020 Pitchayapong Wisawachaiyawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
